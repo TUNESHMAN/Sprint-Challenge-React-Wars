4a3bbf3ca2c43aba7d3d7d0d150b9134c76c554c
Babatunde <babatundea15@gmail.com>
2019-11-08 09:57:04 +0100

    i displayed the info of persons

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card(props) {
+    return (
+        <div>
+            <h3>{props.name}</h3>
+            <h3>{props.skin_color}</h3>
+            
+        </div>
+    )
+}
+
+export default Card
--- starwars/src/components/Container.js
+++ starwars/src/components/Container.js
@@ -1,23 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "./Card";
 
-function Container(){
-const [wars, setWars] = React.useState([])
-    useEffect(() => {
-        axios
-          .get("https://swapi.co/documentation#people")
-          .then(response => {
-            console.log(response);
-            setWars(response.data)
-
-          })
-          .catch(error => {
-            console.log(`This is an error`);
-          });
-      }, []);
-    return(
-        <h2>React is our game</h2>
-    )
+function Container() {
+  const [wars, setWars] = React.useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
+        console.log(response.data.results);
+        setWars(response.data.results);
+      })
+      .catch(error => {
+        console.log(`This is an error`);
+      });
+  }, []);
+  return (
+    <div>
+      {wars.map((element, index) => {
+        return (
+          <Card
+            key={index}
+            name={element.name}
+            height={element.height}
+            mass={element.mass}
+            skin_color={element.skin_color}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-export default Container
+export default Container;
